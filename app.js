9628b45bd4b408a1a9912c1310b01374d49d8e20
sukeesh <vsukeeshbabu@gmail.com>
2017-10-30 23:08:43 +0530

    Add boards and link previews

--- app.js
+++ app.js
@@ -4,15 +4,13 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const redis = require('redis');
-var redis_scanner = require('redis-scanner');
+let unirest = require('unirest');
 
 // Create a redis client
 let client = redis.createClient();
 client.on('connect', function(){
 	console.log("Connected to redis");
 });
-
-redis_scanner.bindScanners(client);
 
 const port = 3000;
 const app = express();
@@ -41,11 +39,59 @@ app.post('/user/search', function(req, res, next){
 			res.render('searchusers', {
 				error: 'User does not exist'
 			});
-		}else{
+		}
+		else{
 			obj.id = id;
-			res.render('details', {
-				user: obj
+			let request = require('request');
+			
+			let link1 = "http://api.proc.link/oembed?url=http%3A%2F%2F" + obj.link1;
+			let link2 = "http://api.proc.link/oembed?url=http%3A%2F%2F" + obj.link2;
+			let link3 = "http://api.proc.link/oembed?url=http%3A%2F%2F" + obj.link3;
+			let link4 = "http://api.proc.link/oembed?url=http%3A%2F%2F" + obj.link4;
+			
+			global.thumb1;
+			global.thumb2;
+			global.thumb3;
+			global.thumb4;
+
+			request(link1, function (error, response, body) {
+			    if (!error && response.statusCode == 200) {
+			      let info = JSON.parse(body);
+			      thumb1 = info;
+			    }
 			});
+
+			// console.log(global.thumb1);
+
+			request(link2, function (error, response, body) {
+			    if (!error && response.statusCode == 200) {
+			      let info = JSON.parse(body);
+			      thumb2 = info;
+			    }
+			});
+
+			request(link3, function (error, response, body) {
+			    if (!error && response.statusCode == 200) {
+			      let info = JSON.parse(body);
+			      thumb3 = info;
+			    }
+			});
+
+			request(link4, function (error, response, body) {
+			    if (!error && response.statusCode == 200) {
+			      let info = JSON.parse(body);
+			      thumb4 = info;
+			    }
+			});
+
+			res.render('details', {
+				user: obj,
+				thumb1: global.thumb1,
+				thumb2: global.thumb2,
+				thumb3: global.thumb3,
+				thumb4: global.thumb4
+			});
+
 		}
 	});
 });
@@ -56,6 +102,7 @@ app.get('/', function(req, res, next){
 		if (err){
 			return console.log(err);
 		}
+
 		res.render('display', {
 			key: keys
 		});
